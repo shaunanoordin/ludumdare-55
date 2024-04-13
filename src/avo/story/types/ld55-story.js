ceaa88c13b4526a94fa073b7314dd4e15165e933
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2024-04-13 23:24:16 +0100

    Create LD55Story

---
+++ src/avo/story/types/ld55-story.js
@@ -0,0 +1,51 @@
+import { POINTER_STATES } from '@avo/constants'
+
+import Story from '../story'
+import ImageAsset from '@avo/image-asset'
+
+import Hero from '@avo/entity/types/hero'
+import Wall from '@avo/entity/types/wall'
+import Ball from '@avo/entity/types/ball'
+import Enemy from '@avo/entity/types/enemy'
+
+import ZeldaControls from '@avo/rule/types/zelda-controls'
+
+export default class LD55Story extends Story {
+  constructor (app) {
+    super(app)
+  }
+
+  get assets () {
+    return {
+      "hero-4dir": new ImageAsset('assets/avo-sprites-2022-05-samiel.png'),
+      "hero-2dir": new ImageAsset('assets/avo-sprites-2022-10-samiel-2dir.png'),
+    }
+  }
+
+  start () {
+    super.start()
+    this.generate_zelda_default()
+  }
+
+  /*
+  Default top-down adventure level.
+   */
+  generate_zelda_default () {
+    const app = this._app
+
+    app.hero = app.addEntity(new Hero(app, 11, 1))
+    app.camera.target = app.hero
+
+    app.addRule(new ZeldaControls(app))
+
+    app.addEntity(new Wall(app, 0, 0, 1, 23))  // West Wall
+    app.addEntity(new Wall(app, 22, 0, 1, 23))  // East Wall
+    app.addEntity(new Wall(app, 1, 0, 21, 1))  // North Wall
+    app.addEntity(new Wall(app, 1, 22, 21, 1))  // South Wall
+
+    app.addEntity(new Ball(app, 8, 6))
+
+    const enemy = app.addEntity(new Enemy(app, 4, 8))
+    enemy.rotation = -45 / 180 * Math.PI
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import AvO from './avo'
+import LD55Story from '@avo/story/types/ld55-story'
 
 var avo
 window.onload = function() {
-  window.avo = new AvO()
+  window.avo = new AvO({ story: LD55Story })
 }
